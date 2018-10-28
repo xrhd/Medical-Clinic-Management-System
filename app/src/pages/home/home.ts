67d14170c018026cbafcb7d264d122385aa4d47b
kengoDev <kengo.startup.dev@gmail.com>
2018-10-28 18:32:46 -0300

    no ionic page

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
