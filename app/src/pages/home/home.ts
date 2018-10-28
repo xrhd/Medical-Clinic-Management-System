5710772e7c2f9eef3603992ac66e4379117b658c
kengoDev <kengo.startup.dev@gmail.com>
2018-10-28 17:37:46 -0300

    auth module

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, private toast: ToastController) {
 
+  }
+
+  ionViewWillLoad() {
+    this.afAuth.authState.subscribe(data => {
+      let message;
+      if(data && data.email && data.uid)
+        message = `Welcome to MedMan, ${data.email}` 
+      else
+        message = 'No authentication details'
+      this.toast
+      .create({
+        message: message,
+        duration: 3000
+      })
+      .present()
+    })
   }
 
 }
