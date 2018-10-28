82a0a6dbc86eb3a40a2e853ade7f6018705338b9
Sergio Augusto <sacbjkengo@gmail.com>
2018-10-28 15:17:41 -0400

    Correcao no user Singin, alteracao nos CSS concentrando os estilos no app.css

--- app/src/pages/singin/singin.ts
+++ app/src/pages/singin/singin.ts
@@ -9,6 +9,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SinginPage {
 
+  private user:{ 
+    name:String; 
+    email:String; 
+    password:String; 
+    confirmPassword:String
+  } = {name:'', email:'',password:'', confirmPassword:''};
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
